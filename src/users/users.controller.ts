395b4726e130645edbe8231d89cdd213480e6e75
Victor Sérgio Silva Barros <victor.silva.barros@avanade.com>
2022-10-26 09:17:37 -0300

    create DTO

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dto/createUser.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
 
     constructor(private readonly usersService:UsersService){}
+
+    @Post()
+    create(@Body() reg: CreateUserDTO){
+        return this.usersService.create();
+    }
 }
